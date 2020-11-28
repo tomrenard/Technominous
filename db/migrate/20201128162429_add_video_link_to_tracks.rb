cb3e2e3c01b5ce551741ba26590b224cc70e822e
Tom Renard <tom.renard@zoho.com>
2020-11-28 17:24:58 +0100

    Add photo and video links to Tracks

---
+++ db/migrate/20201128162429_add_video_link_to_tracks.rb
@@ -0,0 +1,4 @@
+class AddVideoLinkToTracks < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+
+scrape = Scraper.new
+tracks = scrape.scrape_track_urls
+Track.create_from_scraping(tracks)
