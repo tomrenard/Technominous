9d40a2343d4002be177f2962b10d8967381c6cdb
Tom Renard <tom.renard@zoho.com>
2020-11-28 17:01:54 +0100

    Add routes + controller tracks

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,49 @@
+class TracksController < ApplicationController
+  def index
+    @tracks = Track.all
+  end
+
+  def edit
+    @tracks = Tracks.find(params[:id])
+  end
+
+  def update
+    @track = Track.find(params[:id])
+    if @track.update(track_params)
+      redirect_to @track
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+    # @user = @track.user
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    # @track.user = current_user
+    if @track.save
+      redirect_to @track
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @track = Track.find(params[:id])
+    @track.destroy
+    redirect_to tracks_path
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :artist)
+  end
+end
